62ebecc8655f0876b29d3974ff040aec4d1874ae
kalvakotasupriya-cloud <kalvakotasupriya@gmail.com>
2026-01-14 14:37:39 +0530

    Complete CRUD API's for internships

--- server/routes/internshiproutes.js
+++ server/routes/internshiproutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const Internship = require("../models/Internship");
 
 router.get("/", async (req, res) => {
-  res.json([]);
+  try {
+    console.log("GET /internships called"); // 👈 ADD
+    const internships = await Internship.find();
+    res.json(internships);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-
 router.post("/", async (req, res) => {
   try {
+    console.log("Request body:", req.body);
     const internship = new Internship(req.body);
     await internship.save();
     res.status(201).json(internship);
@@ -15,5 +21,26 @@ router.post("/", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-
+router.delete("/:id",async(req,res)=>{
+try{
+    await Internship.findByIdAndDelete(req.params.id);
+    res.json({message:"Internship deleted successfully"});
+}
+catch(error){
+    res.status(500).json({message:error.message});
+}
+});
+// UPDATE internship by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedInternship = await Internship.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedInternship);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 module.exports = router;
